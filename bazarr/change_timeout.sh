f54077dddc5ade272a1404ad3a274a98ffb5d034
Makeshift <Me@Connor-Bell.com>
2020-03-31 14:58:11 +0100

    Bazarr hammers the API's way too often

--- bazarr/change_timeout.sh
+++ bazarr/change_timeout.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/with-contenv bash
-# git is already installed from the base package so we may as well borrow it
-  echo '----------------------------------'
-  echo '|  Replacing Timeouts in Bazarr  |'
-  echo '----------------------------------'
+  echo '----------------------------------------------'
+  echo '|  Replacing Timeouts & Intervals in Bazarr  |'
+  echo '----------------------------------------------'
   echo
 
 find /app/bazarr/ -type f -name "*.py" | xargs sed -E -i 's/timeout=[0-9]{1,3}/timeout=600/g'
+sed -i 's/IntervalTrigger(minutes=1)/IntervalTrigger(hours=6)/g' /app/bazarr/bazarr/scheduler.py
+sed -i 's/IntervalTrigger(minutes=5)/IntervalTrigger\(hours=8\)/g' /app/bazarr/bazarr/scheduler.py
 
 echo Done!
